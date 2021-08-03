2fa15750a1f49082ad341ce0d5099d5abbbf3e0f
d3nd3 <2523452+d3nd3@users.noreply.github.com>
2021-08-03 22:45:32 +0100

    Update launch_sp.sh
    was not sp launch, now fixed.

--- launch_sp.sh
+++ launch_sp.sh
@@ -1,10 +1,7 @@
-LD_LIBRARY_PATH=. ./sof-mp \
+LD_LIBRARY_PATH=. ./sof-bin \
 +set gl_driver /usr/lib/i386-linux-gnu/libGL.so.1 \
 +set menu_key xxxx-xxxx-xxxx-xxxx-xxxx-xxxx \
 +set console 1 \
 +set vid_fullscreen 0 \
 +set gl_finish 1 \
-+set version 1.07fx86F \
-+set protocol 33 \
-+set no_won 1 \
 +set developer 7
