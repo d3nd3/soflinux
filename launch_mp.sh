28472a44f09b3b7a3bb48f76fd018a8ec43922b2
d3nd3 <d3nd3second@gmail.com>
2022-06-10 22:17:06 +0100

    Update launch_mp.sh

--- launch_mp.sh
+++ launch_mp.sh
@@ -1,6 +1,6 @@
 LD_LIBRARY_PATH=. ./sof-mp \
 +set gl_driver /usr/lib/i386-linux-gnu/libGL.so.1 \
-+set menu_key xxxx-xxxx-xxxx-xxxx-xxxx-xxxx \
++set menu_key "xxxx-xxxx-xxxx-xxxx-xxxx-xxxx" \
 +set console 1 \
 +set vid_fullscreen 0 \
 +set gl_finish 1 \
