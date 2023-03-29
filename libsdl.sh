3a668ebb706b7df70775443b32f94b7bae982027
Graham Fenner <d3nd3second@gmail.com>
2023-03-29 07:11:36 +0100

    libsdl

---
+++ libsdl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+INSTALL_DIR=~/.loki/sof-runtime
+
+docker build -t libsdl libsdlcompat-context
+docker create --name tmp-libsdl libsdl
+docker cp -L tmp-libsdl:/sdl12-compat/libSDL-1.2.so.0 ${INSTALL_DIR}/libSDL-1.2.so.0
+docker cp -L tmp-libsdl:/SDL/build/build/.libs/libSDL2-2.0.so.0 ${INSTALL_DIR}/libSDL2-2.0.so.0
+docker rm tmp-libsdl
