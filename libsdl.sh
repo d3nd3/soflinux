2d59da66fb1d4dcdfb2d4ab8944cfebe72fb4815
Marchosias140 <206105839+Marchosias140@users.noreply.github.com>
2026-05-24 21:34:06 -0400

    Update libsdl.sh

--- libsdl.sh
+++ libsdl.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 INSTALL_DIR=~/.loki/sof-runtime
 
-podman build -t libsdl libsdlcompat-context
-podman create --name tmp-libsdl libsdl
-podman cp -L tmp-libsdl:/sdl12-compat/libSDL-1.2.so.0 ${INSTALL_DIR}/libSDL-1.2.so.0
-podman cp -L tmp-libsdl:/SDL/build/build/.libs/libSDL2-2.0.so.0 ${INSTALL_DIR}/libSDL2-2.0.so.0
-podman rm tmp-libsdl
+docker build -t libsdl libsdlcompat-context
+docker create --name tmp-libsdl libsdl
+docker cp -L tmp-libsdl:/sdl12-compat/libSDL-1.2.so.0 ${INSTALL_DIR}/libSDL-1.2.so.0
+docker cp -L tmp-libsdl:/SDL/build/build/.libs/libSDL2-2.0.so.0 ${INSTALL_DIR}/libSDL2-2.0.so.0
+docker rm tmp-libsdl
