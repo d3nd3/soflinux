35455eb167c7584de0f28495d529fb48d4c197fd
Marchosias140 <206105839+Marchosias140@users.noreply.github.com>
2026-05-24 21:38:23 -0400

    Update enter-container.sh

--- docker-run/enter-container.sh
+++ docker-run/enter-container.sh
@@ -15,7 +15,7 @@ fi
 . $(dirname "${BASH_SOURCE[0]}")/pre-run.sh
 LAUNCH_OPTS="/bin/bash"
 
-$TOSUDO podman run -it \
+$TOSUDO docker run -it \
 --platform linux/i386 \
 --env DISPLAY=$DISPLAY \
 --device /dev/dri \
